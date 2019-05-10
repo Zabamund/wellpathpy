157ac637056397ef64e48b3e0b073c8d0bab060c
Zabamund <fracgeol@gmail.com>
2019-05-10 10:46:37 +0200

    Complete python project template
    Populating setup.py and friends in particular pytest is set as driver
    for `python setup.py test`.

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+long_description = """
+To do
+"""
+
+setup(name='wellpathpy',
+      description='Light package to load well deviations',
+      long_description=long_description,
+      author='Robert Leckenby, Brendon Hall, Jørgen Kvalsvik',
+      author_email='fracgeol@gmail.com',
+      url='https://github.com/Zabamund/wellpathpy',
+      packages=['wellpathpy'],
+      license='LGPL-3.0',
+      platforms='any',
+      install_requires=['numpy >=1.10', 'pandas', 'matplotlib'],
+      setup_requires=['setuptools >=28', 'setuptools_scm', 'pytest-runner'],
+      tests_require=['pytest'],
+      )
