49300847097ac8e49331bcc9921e44bc54b156a5
Zabamund <fracgeol@gmail.com>
2019-05-17 13:41:13 +0200

    Move bal_tan method into main module
    Move bal_tan_method to to main module, move return call to start of
    tan_method() to avoid double calling checkarrays().

--- wellpathpy/tan.py
+++ wellpathpy/tan.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from .checkarrays import checkarrays
 
-def tan_method(md, inc, azi, choice = 'avg'):
+def tan_method(md, inc, azi, choice='avg'):
     """
     ToDo
     ----
@@ -10,8 +10,9 @@ def tan_method(md, inc, azi, choice = 'avg'):
         replace `np.insert([tvd, northing, easting], 0, 0)` with
         `np.insert([tvd, northing, easting], 0, <surface location>)`
     """
-    if choice is None:
-        choice = 'avg'
+
+    if choice == 'bal':
+        return bal_tan_method(md, inc, azi)
 
     md, inc, azi = checkarrays(md, inc, azi)
 
@@ -24,27 +25,27 @@ def tan_method(md, inc, azi, choice = 'avg'):
 
     if choice == 'high':
         # extract the lower survey stations
-        inc_choice = inc_r[1:]
-        azi_choice = azi_r[1:]
+        inc = inc_r[1:]
+        azi = azi_r[1:]
     elif choice == 'low':
         # extract the upper survey stations
-        inc_choice = inc_r[:-1]
-        azi_choice = azi_r[:-1]
+        inc = inc_r[:-1]
+        azi = azi_r[:-1]
     elif choice == 'avg':
-        inc_choice = (inc_r[1:] + inc_r[:-1]) / 2
-        azi_choice = (azi_r[1:] + azi_r[:-1]) / 2
+        inc = (inc_r[1:] + inc_r[:-1]) / 2
+        azi = (azi_r[1:] + azi_r[:-1]) / 2
     else:
         msg = 'unknown choice {}, must be one of {}'
-        choices = ['high', 'low', 'avg']
+        choices = ['high', 'low', 'avg', 'bal']
         raise ValueError(msg.format(choice, ' '.join(choices)))
 
-    northing = np.cumsum((md_lower - md_upper) * np.sin(inc_choice) * np.cos(azi_choice))
+    northing = np.cumsum((md_lower - md_upper) * np.sin(inc) * np.cos(azi))
     northing = np.insert(northing, 0, 0)
 
-    easting = np.cumsum((md_lower - md_upper) * np.sin(inc_choice) * np.sin(azi_choice))
+    easting = np.cumsum((md_lower - md_upper) * np.sin(inc) * np.sin(azi))
     easting = np.insert(easting, 0, 0)
 
-    tvd = np.cumsum((md_lower - md_upper) * np.cos(inc_choice))
+    tvd = np.cumsum((md_lower - md_upper) * np.cos(inc))
     tvd = np.insert(tvd, 0, 0)
 
     return tvd, northing, easting
@@ -58,6 +59,18 @@ def high_tan_method(md, inc, azi):
     This method is not recommended as it can make gross tvd and offset
     errors in typical deviated wells.
 
+    Formula
+    -------
+    northing = sum((md_lower - md_upper) * sin(inc_lower) * cos(azi_lower))
+    easting = sum((md_lower - md_upper) * sin(inc_lower) * sin(azi_lower))
+    tvd = sum((md_lower - md_upper) * cos(azi_lower))
+
+    where:
+    md_upper: upper survey station depth MD
+    md_lower: lower survey station depth MD
+    inc_lower: lower survey station inclination in degrees
+    azi_lower: lower survey station azimuth in degrees
+
     Parameters
     ----------
     md: float, measured depth in m or ft
@@ -71,7 +84,7 @@ def high_tan_method(md, inc, azi):
         northing in m or feet,
         easting in m or feet
     """
-    return tan_method(md, inc, azi, choice = 'high')
+    return tan_method(md, inc, azi, choice='high')
 
 def low_tan_method(md, inc, azi):
     """
@@ -82,6 +95,18 @@ def low_tan_method(md, inc, azi):
     This method is not recommended as it can make gross tvd and offset
     errors in typical deviated wells.
 
+    Formula
+    -------
+    northing = sum((md_lower - md_upper) * sin(inc_upper) * cos(azi_upper))
+    easting = sum((md_lower - md_upper) * sin(inc_upper) * sin(azi_upper))
+    tvd = sum((md_lower - md_upper) * cos(azi_upper))
+
+    where:
+    md_upper: upper survey station depth MD
+    md_lower: lower survey station depth MD
+    inc_upper: upper survey station inclination in degrees
+    azi_upper: upper survey station azimuth in degrees
+
     Parameters
     ----------
     md: float, measured depth in m or ft
@@ -95,7 +120,7 @@ def low_tan_method(md, inc, azi):
         northing in m or feet,
         easting in m or feet
     """
-    return tan_method(md, inc, azi, choice = 'low')
+    return tan_method(md, inc, azi, choice='low')
 
 def ave_tan_method(md, inc, azi):
     """
@@ -104,6 +129,20 @@ def ave_tan_method(md, inc, azi):
     bottom of the survey interval before taking their sine and cosine,
     this average angle is used to estimate tvd.
 
+    Formula
+    -------
+    northing = sum((md_lower - md_upper) * sin((inc_lower + inc_upper) / 2) * cos((azi_lower + azi_upper) / 2))
+    easting = sum((md_lower - md_upper) * sin((inc_lower + inc_upper) / 2) * sin((azi_lower + azi_upper) / 2))
+    tvd = sum((md_lower - md_upper) * cos((inc_lower + inc_upper) / 2))
+
+    where:
+    md_upper: upper survey station depth MD
+    md_lower: lower survey station depth MD
+    inc_upper: upper survey station inclination in degrees
+    inc_lower: lower survey station inclination in degrees
+    azi_upper: upper survey station azimuth in degrees
+    azi_lower: lower survey station azimuth in degrees
+
     Parameters
     ----------
     md: float, measured depth in m or ft
@@ -117,4 +156,71 @@ def ave_tan_method(md, inc, azi):
         northing in m or feet,
         easting in m or feet
     """
-    return tan_method(md, inc, azi, choice = 'avg')
+    return tan_method(md, inc, azi, choice='avg')
+
+def bal_tan_method(md, inc, azi):
+    """
+    Calculate TVD using balanced tangential method.
+    This method takes the sines and cosines of the inclination and azimuth
+    at the top and bottom of the survey interval before averaging them,
+    this average angle is used to estimate tvd.
+
+    This will provide a smoother curve than the ave_tan method but requires
+    closely spaced survey stations to avoid errors.
+
+    Formula
+    -------
+    northing = sum(md_lower - md_upper) * ((sin(inc_upper) * cos(azi_upper) + sin(inc_lower) * cos(azi_lower)) / 2)
+    easting = sum(md_lower - md_upper) * ((sin(inc_upper) * sin(azi_upper) + sin(inc_lower) * sin(azi_lower)) / 2)
+    tvd = sum((md_lower - md_upper) * (cos(inc_lower) + cos(inc_upper)) / 2)
+
+    where:
+    md_upper: upper survey station depth MD
+    md_lower: lower survey station depth MD
+    inc_upper: upper survey station inclination in degrees
+    inc_lower: lower survey station inclination in degrees
+    azi_upper: upper survey station azimuth in degrees
+    azi_lower: lower survey station azimuth in degrees
+
+    Parameters
+    ----------
+    md: float, measured depth in m or ft
+    inc: float, well deviation in degrees
+    azi: float, well azimuth in degrees
+
+    Returns
+    -------
+    Deviation converted to TVD, easting, northing
+        tvd in m or feet,
+        northing in m or feet,
+        easting in m or feet
+
+    ToDo
+    ----
+    Implement surface location
+        replace `np.insert([tvd, northing, easting], 0, 0)` with
+        `np.insert([tvd, northing, easting], 0, <surface location>)`
+    """
+    md, inc, azi = checkarrays(md, inc, azi)
+
+    # convert degrees to radians for numpy functions
+    azi_r = np.deg2rad(azi)
+    inc_r = np.deg2rad(inc)
+
+    # extract upper and lower survey stations
+    md_upper, md_lower = md[:-1], md[1:]
+    inc_upper, inc_lower = inc_r[:-1], inc_r[1:]
+    azi_upper, azi_lower = azi_r[:-1], azi_r[1:]
+
+    northing = np.cumsum((md_lower - md_upper) * (np.sin(inc_upper) * np.cos(azi_upper)
+                                                  + np.sin(inc_lower) * np.cos(azi_lower)) / 2)
+    northing = np.insert(northing, 0, 0)
+
+    easting = np.cumsum((md_lower - md_upper) * (np.sin(inc_upper) * np.sin(azi_upper)
+                                                  + np.sin(inc_lower) * np.sin(azi_lower)) / 2)
+    easting = np.insert(easting, 0, 0)
+
+    tvd = np.cumsum((md_lower - md_upper) * (np.cos(inc_lower) + np.cos(inc_upper)) / 2)
+    tvd = np.insert(tvd, 0, 0)
+
+    return tvd, northing, easting
