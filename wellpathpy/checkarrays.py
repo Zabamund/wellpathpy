f29aead364559fcbf377bb922235f15ce5963901
Zabamund <fracgeol@gmail.com>
2019-05-31 14:52:04 +0200

    add check tvd array

--- wellpathpy/checkarrays.py
+++ wellpathpy/checkarrays.py
@@ -47,3 +47,46 @@ def checkarrays(md, inc, azi):
         raise ValueError('md must have strictly increasing values')
 
     return md, inc, azi
+
+def checkarrays_tvd(tvd, northing, easting):
+    """
+    Assure basic preconditions are met, and convert input (tvd, northing, easting) to
+    numpy arrays.
+
+    This function will ensure that:
+
+    - All inputs are convertible to arrays-of-floats, and perform this
+      conversion
+    - All inputs are of the same shape
+
+    Parameters
+    ----------
+    tvd: array_like of float
+        true vertical depth
+    northing: array_like of float
+        north-offset
+    easting: array_like of float
+        east-offset
+
+    Returns
+    -------
+    tvd: array_like of float
+        true vertical depth
+    northing: array_like of float
+        north-offset
+    easting: array_like of float
+        east-offset
+
+    Raises
+    ------
+    ValueError
+        If md, inc, or azi, are of different shapes
+    """
+    tvd = np.asarray(tvd, dtype = np.float)
+    northing = np.asarray(northing, dtype = np.float)
+    easting = np.asarray(easting, dtype = np.float)
+
+    if not (tvd.shape == northing.shape == easting.shape):
+        raise ValueError('tvd, northing, and easting must be the same shape')
+
+    return tvd, northing, easting
