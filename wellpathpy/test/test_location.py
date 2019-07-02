26061ee51e2551c1ba7159133e411103100fadd4
Zabamund <fracgeol@gmail.com>
2019-07-02 15:20:20 +0200

    Changing feet to meters correction in response to issue: "m <-> ft conversion rounding error #7" posted by AlexC84 on github.

--- wellpathpy/test/test_location.py
+++ wellpathpy/test/test_location.py
@@ -10,25 +10,25 @@ well9 = pd.read_csv('./wellpathpy/test/fixtures/well9.csv', sep=",")
 well10 = pd.read_csv('./wellpathpy/test/fixtures/well10.csv', sep=",")
 
 # get data series
-well9_true_md_m = well9['Measured Depth ( ft )'].values / 3.2808 # converting feet to meters
+well9_true_md_m = well9['Measured Depth ( ft )'].values * 0.3048 # converting feet to meters
 well9_true_inc = well9['Inclination ( deg )'].values
 well9_true_azi = well9['Azimuth Grid ( deg )'].values
-well9_true_tvd_m = well9['TVD ( ft )'].values / 3.2808 # converting feet to meters
+well9_true_tvd_m = well9['TVD ( ft )'].values * 0.3048 # converting feet to meters
 well9_true_northing = well9['Northing ( m )'].values
 well9_true_easting = well9['Easting ( m )'].values
 well9_true_surface_northing = 39998.454
 well9_true_surface_easting = 655701.278
-well9_true_datum_elevation = 100 / 3.2808 # converting feet to meters
+well9_true_datum_elevation = 100 * 0.3048 # converting feet to meters
 
-well10_true_md_m = well10['Measured Depth ( ft )'].values / 3.2808 # converting feet to meters
+well10_true_md_m = well10['Measured Depth ( ft )'].values * 0.3048 # converting feet to meters
 well10_true_inc = well10['Inclination ( deg )'].values
 well10_true_azi = well10['Azimuth Grid ( deg )'].values
-well10_true_tvd_m = well10['TVD ( ft )'].values / 3.2808 # converting feet to meters
+well10_true_tvd_m = well10['TVD ( ft )'].values * 0.3048 # converting feet to meters
 well10_true_northing = well10['Northing ( m )'].values
 well10_true_easting = well10['Easting ( m )'].values
 well10_true_surface_northing = 40004.564
 well10_true_surface_easting = 655701.377
-well10_true_datum_elevation = 100 / 3.2808 # converting feet to meters
+well10_true_datum_elevation = 100 * 0.3048 # converting feet to meters
 
 def test_wellhead():
     # test well9
