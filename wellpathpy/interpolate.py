a50ec5b7b1b27f0f06758235dc54e6b56d6eb79a
Zabamund <fracgeol@gmail.com>
2019-07-09 17:05:35 +0200

    Add first tests for interpolate.py
    test for: input arrays contain no NaN values
    test for: md_step and tvd_step are of type int
    
    md_step and tvd_step tests may not be correct
    
    ToDo: tests for interpolation

--- wellpathpy/interpolate.py
+++ wellpathpy/interpolate.py
@@ -32,6 +32,9 @@ def interpolate_deviation(md, inc, azi, md_step=1):
         if np.isnan(input_array).any():
             raise ValueError('md, inc and azi cannot contain NaN values.')
 
+    if not isinstance(md_step, int):
+        raise TypeError('md_step must be int')
+
     new_md = np.arange(md.min(), md.max(), md_step)
 
     f_inc = interpolate.interp1d(md, inc)
@@ -72,6 +75,9 @@ def interpolate_position(easting, northing, tvd, tvd_step=1):
     for input_array in [tvd, northing, easting]:
         if np.isnan(input_array).any():
             raise ValueError('tvd, northing and easting cannot contain NaN values.')
+
+    if not isinstance(tvd_step, int):
+        raise TypeError('tvd_step must be int')
 
     new_tvd = np.arange(tvd.min(), tvd.max(), tvd_step)
 
---
+++ wellpathpy/test/test_interpolate.py
@@ -0,0 +1,65 @@
+import pytest
+import numpy as np
+
+from ..interpolate import interpolate_deviation, interpolate_position
+
+# input arrays contain no NaN values
+def test_inter_dev_md_throws():
+    with pytest.raises(ValueError):
+        _ = interpolate_deviation(md=np.array([1,2,np.nan]),
+                                inc=np.array([1,2,3]),
+                                azi=np.array([1,2,3]),
+                                md_step=1)
+
+def test_inter_dev_inc_throws():
+    with pytest.raises(ValueError):
+        _ = interpolate_deviation(md=np.array([1,2,3]),
+                                inc=np.array([1,2,np.nan]),
+                                azi=np.array([1,2,3]),
+                                md_step=1)
+
+def test_inter_dev_azi_throws():
+    with pytest.raises(ValueError):
+        _ = interpolate_deviation(md=np.array([1,2,3]),
+                                inc=np.array([1,2,3]),
+                                azi=np.array([1,2,np.nan]),
+                                md_step=1)
+
+def test_inter_pos_md_throws():
+    with pytest.raises(ValueError):
+        _ = interpolate_position(tvd=np.array([1,2,np.nan]),
+                                easting=np.array([1,2,3]),
+                                northing=np.array([1,2,3]),
+                                tvd_step=1)
+
+def test_inter_pos_inc_throws():
+    with pytest.raises(ValueError):
+        _ = interpolate_position(tvd=np.array([1,2,3]),
+                                easting=np.array([1,2,np.nan]),
+                                northing=np.array([1,2,3]),
+                                tvd_step=1)
+
+def test_inter_pos_azi_throws():
+    with pytest.raises(ValueError):
+        _ = interpolate_position(tvd=np.array([1,2,3]),
+                                easting=np.array([1,2,3]),
+                                northing=np.array([1,2,np.nan]),
+                                tvd_step=1)
+
+# md_step and tvd_step are of type int
+def test_inter_dev_md_step_throws():
+    with pytest.raises(TypeError):
+        _ = interpolate_deviation(md=np.array([1,2,3]),
+                                inc=np.array([1,2,3]),
+                                azi=np.array([1,2,3]),
+                                md_step=1.0)
+
+def test_inter_dev_tvd_step_throws():
+    with pytest.raises(TypeError):
+        _ = interpolate_position(tvd=np.array([1,2,3]),
+                                easting=np.array([1,2,3]),
+                                northing=np.array([1,2,3]),
+                                tvd_step=1.0)
+
+
+# interpolation is close to input data
