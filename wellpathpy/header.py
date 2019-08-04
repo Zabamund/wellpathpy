ad1c7626c0996f1baf21b5efca8372cfb8b45b2f
Zabamund <fracgeol@gmail.com>
2019-08-04 10:21:03 +0200

    Tutorial skeleton for API model
    update docstring of header to reflect actual return type
    
    create skeleton notebook tutorial using OOP approach

--- wellpathpy/header.py
+++ wellpathpy/header.py
@@ -1,8 +1,8 @@
 def get_header(datum='kb', units='m', elevation=0.):
     """
     Record deviation header information needed for depth
-    reference calculation into DataFrame.
-    
+    reference calculation into dict.
+
     Parameters
     ----------
     datum: str, default 'kb', {'kb', 'dfe', 'rt'}
@@ -12,10 +12,10 @@ def get_header(datum='kb', units='m', elevation=0.):
     units: str, default 'm', {'m', 'ft'}
            'm' (metres),
            'ft' (feet)
-    elevation: float, default 0., 
-           <datum> <elevation> in <units> 
+    elevation: float, default 0.,
+           <datum> <elevation> in <units>
            above mean sea level
-    
+
     Returns
     -------
     dict
@@ -23,13 +23,13 @@ def get_header(datum='kb', units='m', elevation=0.):
     """
     if datum not in {'kb', 'dfe', 'rt'}:
         raise ValueError('datum must be kb, dfe or rt')
-    
+
     if units not in {'m', 'ft'}:
         raise ValueError('units must be m or ft')
- 
+
     try:
         elevation = float(elevation + 0)
     except TypeError:
         raise TypeError('elevation must be float')
-        
+
     return {'datum': datum, 'units': units, 'elevation': elevation}
