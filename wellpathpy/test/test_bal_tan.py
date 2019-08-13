cea455c672ac54b52c525df31a2fc0cf106c96a4
Zabamund <fracgeol@gmail.com>
2019-08-13 17:06:14 +0200

    Update function name with correct name

--- wellpathpy/test/test_bal_tan.py
+++ wellpathpy/test/test_bal_tan.py
@@ -1,27 +1,27 @@
 import pytest
 import numpy as np
 
-from ..tan import bal_tan_method
+from ..tan import balanced_tan
 
 # inputs are array-like
 def test_md_throws():
     with pytest.raises(ValueError):
-        _ = bal_tan_method(md='lmkcde', inc=[1,2,3], azi=[1,2,3])
+        _ = balanced_tan(md='lmkcde', inc=[1,2,3], azi=[1,2,3])
 
 def test_inc_throws():
     with pytest.raises(ValueError):
-        _ = bal_tan_method(md=[1,2,3], inc='adsda', azi=[1,2,3])
+        _ = balanced_tan(md=[1,2,3], inc='adsda', azi=[1,2,3])
 
 def test_azi_throws():
     with pytest.raises(ValueError):
-        _ = bal_tan_method(md=[1,2,3], inc=[1,2,3], azi='kjnef')
+        _ = balanced_tan(md=[1,2,3], inc=[1,2,3], azi='kjnef')
 
 # inputs are same length
 def test_input_lengths_throws():
     with pytest.raises(ValueError):
-        _ = bal_tan_method(md=[1,2,3], inc=[1,2,3], azi=[1,2])
+        _ = balanced_tan(md=[1,2,3], inc=[1,2,3], azi=[1,2])
 
 # md array increases strictly at each step
 def test_increasing_md_throws():
     with pytest.raises(ValueError):
-        _ = bal_tan_method(md=[1,1,3], inc=[1,2,3], azi=[1,2,3])
+        _ = balanced_tan(md=[1,1,3], inc=[1,2,3], azi=[1,2,3])
