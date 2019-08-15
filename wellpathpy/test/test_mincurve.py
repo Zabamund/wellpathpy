be667a1a433d2a59fcb80517d40e66068bdd3247
Zabamund <fracgeol@gmail.com>
2019-08-15 12:14:02 +0200

    remove unsused comment

--- wellpathpy/test/test_mincurve.py
+++ wellpathpy/test/test_mincurve.py
@@ -26,11 +26,6 @@ def test_increasing_md_throws():
     with pytest.raises(ValueError):
         _ = minimum_curvature(md=[1,1,3], inc=[1,2,3], azi=[1,2,3])
 
-# get units for dls
-# def test_wrong_units_throws():
-#     with pytest.raises(ValueError):
-#         _ = minimum_curvature(md=[1,2,3], inc=[1,2,3], azi=[1,2,3], md_units='feet')
-
 # get normalising for dls
 def test_bad_normalising_throws():
     with pytest.raises(TypeError):
