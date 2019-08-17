47e63e96b1d3309889f2e7e1dd9fed952bbb5d14
Zabamund <fracgeol@gmail.com>
2019-08-17 16:35:43 +0200

    Add __all__ attribute for documentatin autobuild
    Add the __all__ attribute listing all loaded functions so that
    they are picked up by documentation

--- wellpathpy/__init__.py
+++ wellpathpy/__init__.py
@@ -4,6 +4,25 @@ try:
 except pkg_resources.DistributionNotFound:
     pass
 
+__all__ = [
+    'read_header_json',
+    'read_csv',
+    'unit_convert',
+    'minimum_curvature',
+    'radius_curvature',
+    'high_tan',
+    'low_tan',
+    'balanced_tan',
+    'average_tan',
+    'loc_to_wellhead',
+    'loc_to_zero',
+    'loc_to_tvdss',
+    'resample_deviation',
+    'resample_position',
+    'deviation_to_csv',
+    'position_to_csv'
+    ]
+
 from .convert import unit_convert
 from .header import read_header_json
 from .interpolate import resample_deviation, resample_position
