332e94566d7eb07ce98dad7ae2f95efb2fce8848
Zabamund <fracgeol@gmail.com>
2019-10-01 16:05:08 +0200

    Check geometry.tangent domain properties
    The tangent of inclination and azimuth points has some properties
    that must be respected. These tests verifies two such properties:
    for an inclination value of zero, the tangent is always vertical,
    and irrespective of the input values of inclination and azimuth,
    the tangent outputs (vd, northing and easting) are always within
    the allowed range.

---
+++ wellpathpy/test/test_geometry.py
@@ -0,0 +1,16 @@
+import pytest
+from hypothesis import given
+from hypothesis.strategies import floats
+
+from ..geometry import tangent
+
+@given(floats(allow_nan=False, allow_infinity=False), floats(allow_nan=False, allow_infinity=False))
+def test_unit_vector_domain(inc, azi):
+    vd, northing, easting = tangent(inc, azi)
+    assert -1 <= vd <= 1
+    assert -1 <= northing <= 1
+    assert -1 <= easting <= 1
+
+@given(floats(allow_nan=False, allow_infinity=False))
+def test_zero_inc_yields_vertical_nev(azi):
+    assert tangent(0.0, azi) == (1, 0, 0)
