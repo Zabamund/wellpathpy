acf78fa15a84403861234204f823f542790074ea
Zabamund <fracgeol@gmail.com>
2019-10-04 09:58:24 +0200

    Check geometry.tangent cardinal points behaviour
    The tangent of inclination and azimuth points has some properties
    that must be respected. These tests verify that for the four
    cardinal points and the up and down orientations, all the
    resulting tangents match the expected values.

--- wellpathpy/test/test_geometry.py
+++ wellpathpy/test/test_geometry.py
@@ -1,6 +1,7 @@
 import pytest
 from hypothesis import given
 from hypothesis.strategies import floats
+from pytest import approx
 
 from ..geometry import tangent
 
@@ -21,3 +22,19 @@ def test_zero_azi_fixes_easting(inc):
     assert -1 <= vd <= 1
     assert -1 <= northing <= 1
     assert easting == 0
+
+@given(floats(allow_nan=False, allow_infinity=False))
+def test_180_inc_yields_vertical_nev(azi):
+    assert tangent(180., azi) == (-1, approx(0), approx(0))
+
+def test_n_hor_yields_north_nev():
+    assert tangent(90., 0) == (approx(0), 1, approx(0))
+
+def test_s_hor_yields_south_nev():
+    assert tangent(90., 180.) == (approx(0), -1, approx(0))
+
+def test_e_hor_yields_east_nev():
+    assert tangent(90., 90.) == (approx(0), approx(0), 1)
+
+def test_w_hor_yields_west_nev():
+    assert tangent(90., 270.) == (approx(0), approx(0), -1)
