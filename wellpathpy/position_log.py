81df8e98b7c0b7a2da2dfeb3c338de6965319bb8
Zabamund <fracgeol@gmail.com>
2021-03-01 12:49:47 +0100

    Remove redundant methods from radius_curvature class
    The resample and deviation methods are not needed as a call to them
    will be routed to the position_log methods with same name and
    already raise NotImplementedError

--- wellpathpy/position_log.py
+++ wellpathpy/position_log.py
@@ -378,9 +378,4 @@ class radius_curvature(position_log):
     def copy(self):
         l = radius_curvature(self.source, np.copy(self.depth), np.copy(self.northing), np.copy(self.easting))
         return l
-
-    def resample(self, depths):
-        raise NotImplementedError
-    
-    def deviation(self):
-        raise NotImplementedError
+        
